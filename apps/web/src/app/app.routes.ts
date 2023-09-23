1d51d9c7954dd4421a744c11b34b467735698288
Aleksandr Serenko <fafnur@yandex.ru>
2023-09-23 12:25:17 +0700

    [web][ui] Adding footer component

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,47 +1,48 @@
 import { Route } from '@angular/router';
 
 import { canAuth, canLogged } from '@flashcards/web/auth/guards';
+import { FooterComponent } from '@flashcards/web/ui/footer';
 import { LayoutComponent } from '@flashcards/web/ui/layout';
 
 export const appRoutes: Route[] = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     component: LayoutComponent,
     children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
       // {
       //   path: '',
       //   component: HeaderComponent,
       //   outlet: 'header',
       // },
-      // {
-      //   path: '',
-      //   component: FooterComponent,
-      //   outlet: 'footer',
-      // },
+      {
+        path: '',
+        component: FooterComponent,
+        outlet: 'footer',
+      },
       {
         path: 'dashboard',
         canActivate: [canLogged],
         loadComponent: () => import('@flashcards/web/dashboard/page').then((modules) => modules.DashboardPageComponent),
       },
       {
-        path: 'auth',
-        canActivate: [canAuth],
-        loadChildren: () => import('./routes/auth.routes').then((modules) => modules.authRoutes),
-      },
-      {
         path: 'users',
         canActivate: [canLogged],
         loadChildren: () => import('./routes/users.routes').then((modules) => modules.usersRoutes),
       },
-      {
-        path: '',
-        loadChildren: () => import('./routes/errors.routes').then((modules) => modules.errorsRoutes),
-      },
     ],
+  },
+  {
+    path: 'auth',
+    canActivate: [canAuth],
+    loadChildren: () => import('./routes/auth.routes').then((modules) => modules.authRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./routes/errors.routes').then((modules) => modules.errorsRoutes),
   },
 ];
---
+++ libs/web/ui/footer/src/lib/footer.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { ContainerComponent } from '@flashcards/web/ui/container';
+
+import { CopyrightComponent } from './copyright/copyright.component';
+
+@Component({
+  selector: 'flashcards-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [ContainerComponent, CopyrightComponent],
+})
+export class FooterComponent {}

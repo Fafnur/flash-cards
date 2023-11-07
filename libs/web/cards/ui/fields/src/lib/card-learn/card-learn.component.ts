bd9f3b39b4258dd8920ba705f46c87e41db3f054
Aleksandr Serenko <fafnur@yandex.ru>
2023-11-07 09:58:29 +0700

    [web][i18n] Mark translations

--- libs/web/cards/ui/fields/src/lib/card-learn/card-learn.component.ts
+++ libs/web/cards/ui/fields/src/lib/card-learn/card-learn.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
+import { GroupLangComponent } from '@flashcards/web/groups/ui/fields';
+
 @Component({
   selector: 'flashcards-card-learn',
   templateUrl: './card-learn.component.html',
   styleUrls: ['./card-learn.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatCheckboxModule, ReactiveFormsModule],
+  imports: [MatCheckboxModule, ReactiveFormsModule, GroupLangComponent],
 })
 export class CardLearnComponent {
   @Input({ required: true }) control!: FormControl<boolean>;
